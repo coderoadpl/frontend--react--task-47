5fb821c8375d4110b07ae1700b40ff1f2465e260
Mateusz Choma <kontakt@coderoad.pl>
2021-07-22 08:31:58 +0200

    Added icon from npm package

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
 export const App = () => {
   return (
     <div>
-      CodeRoad APP
+      <FontAwesomeIcon
+        icon={faCoffee}
+      />
     </div>
   )
 }
